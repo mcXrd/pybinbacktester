9722b522b848af5f7cc70e732ed24d7a6135dc41
mcXrd <matejka.vaclav@gmail.com>
2019-01-15 19:59:10 +0100

    feature/generate_market_data_hdf_command

--- dj/dj/settings.py
+++ dj/dj/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+HDF_STORAGE_FOLDER = 'static_storage'
+HDF_STORAGE_PATH = os.path.join(BASE_DIR, HDF_STORAGE_FOLDER)
 
 
 # Quick-start development settings - unsuitable for production
